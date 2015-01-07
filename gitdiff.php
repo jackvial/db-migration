da029127a802d293174ea171ba7fe6ccac011cd4
Jack Vial <vialjack@mail.com>
2015-01-07 17:01:27 -0500

    Removed unused files

--- gitdiff.php
+++ gitdiff.php
@@ -1,5 +1,14 @@
 #!/usr/bin/php
 <?php
     $newFiles = shell_exec('git diff HEAD^ HEAD --name-only includes/');
-    echo $newFiles;
+    var_dump($newFiles);
+
+    $filesArray = preg_split('/[\r\n]+/', $newFiles, -1, PREG_SPLIT_NO_EMPTY);
+    var_dump($filesArray);
+        
+        
+    foreach($filesArray as $fileName){
+       $fileContents = file_get_contents($fileName);
+       echo $fileContents."\n";
+    }
 ?>
