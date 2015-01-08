12eb078a91918894c3e8b52f53126f5322f21103
Jack Vial <vialjack@mail.com>
2015-01-08 09:08:39 -0500

    Added test setup and teardown

--- tests/unit/MigrateTest.php
+++ tests/unit/MigrateTest.php
@@ -2,32 +2,34 @@
 require_once('migrate.php');
 class MigrateTest extends \PHPUnit_Framework_TestCase
 {
+    // Contains the object handle    
+    var $migrate;
+
     protected function setUp()
     {
+        $this->migrate = new Migrate();
     }
 
     protected function tearDown()
     {
+        // Delete the class instance
+        unset($this->migrate);
     }
 
-    // tests
     public function testInitMethod()
     {
-        $Migrate = new Migrate();
-        $this->assertTrue($Migrate->init() === 'is alive!');  
+        $this->assertTrue($this->migrate->init() === 'is alive!');  
     }
 
     public function testGitDiff()
     {
-        $Migrate = new Migrate();
-        $this->assertTrue(is_string($Migrate->gitDiff()));  
-        $this->assertEquals(trim($Migrate->gitDiff()), 'includes/update_tables.sql');  
+        $this->assertTrue(is_string($this->migrate->gitDiff()));  
+        $this->assertEquals(trim($this->migrate->gitDiff()), 'includes/update_tables.sql');  
     }
 
     public function testSplitStringReturnsArray()
     {
-        $Migrate = new Migrate();
-        $this->assertTrue(is_array($Migrate->splitOnNewLine($Migrate->gitDiff()))); 
+        $this->assertTrue(is_array($this->migrate->splitOnNewLine($this->migrate->gitDiff()))); 
     }
 
 }
