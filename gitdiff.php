dd405aceeae5a2ea84944b452401af5167fae2ba
Jack Vial <vialjack@mail.com>
2015-01-07 19:46:41 -0500

    Testing write to db

--- gitdiff.php
+++ gitdiff.php
@@ -4,24 +4,22 @@
     $filesArray = preg_split('/[\r\n]+/', $newFiles, -1, PREG_SPLIT_NO_EMPTY);
     $timeStampFileName = array();
     foreach($filesArray as $key => $fileName){
-       $sqlScripts[$key] = file_get_contents($fileName);
        $timeStampFileName[date("H:i:s d/m/y", filemtime($fileName))] = $fileName;
        //echo $fileContents."\n";
     }
     
+    $finScripts = 'finished_scripts.txt';
+    $finContents = file_get_contents($finScripts);
+    $finFileArray = explode(',', $finContents); 
+    print_r($finFileArray);
     // Sort chronologically
-    //$timeStampFileName = ksort($timeStampFileName); 
-    print_r($timeStampFileName);
-    print_r($sqlScripts);
 
     try {
         $dbh = new PDO('mysql:host=localhost;dbname=migration_scripts', 'root', 'Welcome1');
-        foreach($sqlScripts as $script) {
-            $result = $dbh->query($script);
-            foreach($result as $row){
-                print_r($row);
-            }
-            //print_r($result);
+        foreach($timeStampFileName as $key => $fileName) {
+            $result = $dbh->query(file_get_contents($fileName));
+            file_put_contents('finished_scripts.txt', $fileName.',', FILE_APPEND);
+            
         }
         $dbh = null;
     } catch (PDOException $e) {
