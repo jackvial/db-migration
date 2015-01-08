3cbe85a266e40af32ff0afde26450f7a3394eb3b
Jack Vial <vialjack@mail.com>
2015-01-07 19:18:32 -0500

    Added fruit file

--- gitdiff.php
+++ gitdiff.php
@@ -1,14 +1,31 @@
 #!/usr/bin/php
 <?php
     $newFiles = shell_exec('git diff HEAD^ HEAD --name-only includes/');
-    var_dump($newFiles);
-
     $filesArray = preg_split('/[\r\n]+/', $newFiles, -1, PREG_SPLIT_NO_EMPTY);
-    var_dump($filesArray);
-        
-        
-    foreach($filesArray as $fileName){
-       $fileContents = file_get_contents($fileName);
-       echo $fileContents."\n";
+    $timeStampFileName = array();
+    foreach($filesArray as $key => $fileName){
+       $sqlScripts[$key] = file_get_contents($fileName);
+       $timeStampFileName[date("H:i:s d/m/y", filemtime($fileName))] = $fileName;
+       //echo $fileContents."\n";
+    }
+    
+    // Sort chronologically
+    //$timeStampFileName = ksort($timeStampFileName); 
+    print_r($timeStampFileName);
+    print_r($sqlScripts);
+
+    try {
+        $dbh = new PDO('mysql:host=localhost;dbname=migration_scripts', 'root', 'Welcome1');
+        foreach($sqlScripts as $script) {
+            $result = $dbh->query($script);
+            foreach($result as $row){
+                print_r($row);
+            }
+            //print_r($result);
+        }
+        $dbh = null;
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        die();
     }
 ?>
---
+++ includes/insert_fruit.sql
@@ -0,0 +1,3 @@
+INSERT INTO fruit (name, color) VALUES ('Orange', 'Orange');
+INSERT INTO fruit (name, color) VALUES ('Banana', 'Yellow');
+INSERT INTO fruit (name, color) VALUES ('Plum', 'Purple');
