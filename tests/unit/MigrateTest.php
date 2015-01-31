ca375aa13e965f5b058ce10e6a021a16c0b45c33
Jack Vial <vialjack@mail.com>
2015-01-31 12:13:32 -0500

    Testing pre-commit

--- tests/unit/MigrateTest.php
+++ tests/unit/MigrateTest.php
@@ -20,7 +20,7 @@ class MigrateTest extends \PHPUnit_Framework_TestCase
 
     public function testGitDiff()
     {
-        // Testing test includes again
+        // Testing test includes again and again
         $test_directory = 'test_includes/';
         $expected_result = 'Mtest_includes/drop_fruit.sqlMtest_includes/insert_fruit.sqlMtest_includes/update_fruit.sql';
 
